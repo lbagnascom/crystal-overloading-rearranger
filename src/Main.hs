3113bd5825ad9ae7dd43700557b7578909c5607b
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-04-24 12:33:30 -0300

    WIP: run Crystal samples and compare results

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 module Main (main) where
 
 import Data.Text (pack)
+import GHC.IO.Exception (ExitCode)
 import PPrint (printProgram)
-import Parser (parseProgram)
+import Parser (CrystalProgram, parseProgram)
 import Rearranger (rearrangeSlots)
-import System.Directory (createDirectory)
+import System.Directory (createDirectory, doesDirectoryExist)
 import System.Environment (getArgs)
 import System.FilePath (dropExtension, splitFileName, (<.>), (</>))
+import System.Process (createProcess, shell, waitForProcess)
 import Text.Megaparsec (parse)
 
 main :: IO ()
@@ -21,11 +23,39 @@ main = do
         Left _ ->
           putStrLn "Parser failed"
         Right out -> do
-          let indexed = zip [1 ..] (rearrangeSlots out)
           let outputDir = dir </> fileName
-          createDirectory outputDir
-          mapM_
-            (\(i, progr) -> writeFile (outputDir </> show i <.> "cr") (printProgram progr))
-            indexed
+          unlessIO (doesDirectoryExist outputDir) (createDirectory outputDir)
+          samples <- createPrograms (rearrangeSlots out) outputDir
+          _ <- runFiles samples
+          -- putStrLn $ "Outputs are " ++ show outputs
+          pure ()
     _ ->
       putStrLn "Usage: cabal run crystal-parser -- <input-file>"
+
+createPrograms :: [CrystalProgram] -> String -> IO [String]
+createPrograms ps dir = mapM (\(i, p) -> createProgram i p) $ zip [1 :: Int ..] ps
+  where
+    createProgram i p = do
+      let name = dir </> show i <.> "cr"
+      writeFile name $ printProgram p
+      pure name
+
+data ProcessResult = ProcessResult
+  { prExitCode :: ExitCode,
+    prStdout :: String,
+    prStderr :: String
+  }
+  deriving (Show, Eq)
+
+runFiles :: [String] -> IO [ProcessResult]
+runFiles fileNames = mapM runFile fileNames
+  where
+    runFile fileName = do
+      (_, _, _, ph) <- createProcess (shell $ "crystal run " ++ fileName)
+      exitCode <- waitForProcess ph
+      pure $ ProcessResult {prExitCode = exitCode, prStderr = "", prStdout = ""}
+
+unlessIO :: IO Bool -> IO () -> IO ()
+unlessIO condition action = do
+  b <- condition
+  if b then pure () else action
