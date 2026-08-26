54460d7f227be8023b9ccd21eec6e474e3f4bccd
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-08-26 18:02:07 -0300

    Add new test for class and modules type resolution

--- test/Spec.hs
+++ test/Spec.hs
@@ -463,6 +463,22 @@ main = hspec $ do
             end
             |]
 
+      otroAst :: UnresolvedAst
+      otroAst =  fromRight [] $ parse
+            parseProgram
+            ""
+            [__i|
+            module M
+            end
+
+            class A
+              include M
+            end
+
+            class B < A
+            end
+            |]
+
       supuestoRes :: ResolvedAst
       supuestoRes = [
         FunctionStmt (
@@ -484,3 +500,21 @@ main = hspec $ do
     in do
       it "probando type resolver" $
          resolveTypes algunAst `shouldBe` supuestoRes
+      it "funciona para clases y modulos" $
+         resolveTypes otroAst `shouldBe` [
+           ModuleStmt (
+             Module {
+               moduleName = TIdentifier "M",
+               moduleMethods = []}),
+           ClassStmt (
+             Class {
+               className = TIdentifier "A",
+               classSuper = TypeRef {tRefName = TIdentifier "Reference", tRefType = Fix (TClass (Class {className = TIdentifier "Reference", classSuper = TypeRef {tRefName = TIdentifier "Object", tRefType = Fix TString}, classModules = [], classMethods = []}))},
+               classModules = [TypeRef {tRefName = TIdentifier "M", tRefType = Fix (TModule (Module {moduleName = TIdentifier "M", moduleMethods = []}))}],
+               classMethods = []}),
+           ClassStmt (
+             Class {
+               className = TIdentifier "B",
+               classSuper = TypeRef {tRefName = TIdentifier "A", tRefType = Fix (TClass (Class {className = TIdentifier "A", classSuper = TypeRef {tRefName = TIdentifier "Reference", tRefType = Fix (TClass (Class {className = TIdentifier "Reference", classSuper = TypeRef {tRefName = TIdentifier "Object", tRefType = Fix TString}, classModules = [], classMethods = []}))}, classModules = [TypeRef {tRefName = TIdentifier "M", tRefType = Fix (TModule (Module {moduleName = TIdentifier "M", moduleMethods = []}))}], classMethods = []}))},
+               classModules = [],
+               classMethods = []})]
