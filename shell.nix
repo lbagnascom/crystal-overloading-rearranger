beefad31b202fc59fb8a2c174ebe093883199f95
Lautaro Bagnasco Muguillo <tatibagnasco@gmail.com>
2026-09-03 14:37:32 -0300

    Update direnv version and devenv.lock

--- shell.nix
+++ shell.nix
@@ -5,8 +5,7 @@
 # Then replace the sha256 with the output.
 {
   pkgs ? import (fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/a3116115851d68b8952a2a4221cc25a84e56b532.tar.gz
-";
+    url = "https://github.com/NixOS/nixpkgs/archive/a3116115851d68b8952a2a4221cc25a84e56b532.tar.gz";
     sha256 = "0qbzjra0z5v7fwzji7j2h7g632a5zg2934v3j4555n6xnwsx9sv7";
   }) { },
 }:
